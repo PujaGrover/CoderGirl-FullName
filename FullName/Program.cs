ed1260cab0c362dcec8ef24b77ef63d98124b72a
Puja Grover <puja_grover@yahoo.com>
2019-03-04 18:43:30 -0600

    Week6HwFullName, commit, push and sync Issues: Test does not Run. Code works fine

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -7,14 +7,25 @@ namespace FullName
         public static void Main()
         {
             // TODO: Ask the user for their first name and then their last name.
-
             // TODO: Call a method called FullName that returns their full name.
-
             // TODO: Output their full name.
 
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine().Trim();
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine().Trim();
+
+            Console.WriteLine(FullName(firstName, lastName));
             Console.ReadLine();
         }
 
         // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
+        private static string FullName(string firstName, string lastName)
+        {
+            string Name = "";
+            Name = firstName + " " + lastName;
+            return Name;
+        }
     }
+    
 }
